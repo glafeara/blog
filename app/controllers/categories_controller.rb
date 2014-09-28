993e6f41ad2198e6cd555695103a3b3aca85aac0
glafeara <sidorov.grigorii@gmail.com>
2014-09-28 20:12:07 +0400

    Update categories_controller.rb
    Adjustment tabs.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,7 @@ class CategoriesController < ApplicationController
   before_action :is_admin, only: [ :index, :destroy, :new, :edit  ]
 
   def index
-  	@categories = Category.all
+    @categories = Category.all
   end
 
   def new
@@ -12,36 +12,35 @@ class CategoriesController < ApplicationController
   end
 
   def create
-  	@category = Category.create(category_params)
-  	if @category.errors.empty?
-  	  redirect_to @category
-  	else
-  	  render 'new'
-  	end
+    @category = Category.create(category_params)
+    if @category.errors.empty?
+      redirect_to @category
+    else
+      render 'new'
+    end
   end
 
   def edit
-  	find_category
+    find_category
   end
 
   def update
-  	find_category.update(category_params)
-  	if @category.errors.empty?
-  	  redirect_to @category
-  	else
+    find_category.update(category_params)
+    if @category.errors.empty?
+      redirect_to @category
+    else
       render 'edit'
     end
   end
 
   def show
-  	find_category
+    find_category
   end
 
   def destroy
-  	find_category.destroy
-  	redirect_to categories_path
+    find_category.destroy
+    redirect_to categories_path
   end
-
 
   private
 
