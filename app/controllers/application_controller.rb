9dbcea45e631c10db0c669558f3599d978f59f83
Grigoriy <sidorov.grigorii@gmail.com>
2014-09-28 21:07:42 +0400

    Update application_controller.rb
    MORE correction tab in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,19 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-	protect_from_forgery with: :exception
-	before_action :get_categories
+  protect_from_forgery with: :exception
+  before_action :get_categories
 
-	include SessionsHelper
+  include SessionsHelper
 
-	private
+  private
 
-		def get_categories
-			@all_categories = Category.all
-		end
+  def get_categories
+    @all_categories = Category.all
+  end
 
-		def get_categories
-			@all_categories = Category.all
-		end
-		
+  def get_categories
+    @all_categories = Category.all
+  end
+ 
 end
