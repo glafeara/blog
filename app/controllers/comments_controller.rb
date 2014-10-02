1444a225eae6c9db7f42f8be709d129405d1ffa6
glafeara <sidorov.grigorii@gmail.com>
2014-10-02 12:28:29 +0400

    Deleting comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,8 @@ class CommentsController < ApplicationController
 
   before_action :signed
   before_action :premission_for_posts
-  before_action :add_user_id
+  before_action :add_user_id, except: :destroy
+  before_action :this_is_his_comment, only: :destroy
 
   def create
     @article = Article.find(params[:article_id])
@@ -34,8 +35,21 @@ class CommentsController < ApplicationController
       params.require(:comment).permit(:comment, :user_id)
     end
 
-   def add_user_id
-     params[:comment][:user_id] = current_user.id
-   end
+    def add_user_id
+      params[:comment][:user_id] = current_user.id
+    end
+
+    def set_comment
+      @article = Article.find(params[:article_id])
+      @comment = @article.comments.find(params[:id])
+    end
+
+
+  def this_is_his_comment
+    if @current_user.id != set_comment.user_id && !@current_user.isadmin
+      flash[:alert] = 'Вы не можете удалять чужие комментарии!'
+      redirect_to @article
+    end
+  end
 
 end
